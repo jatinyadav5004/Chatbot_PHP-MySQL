b629380b054194aba6e49589a646c3a681f7783a
jatinyadav5004 <jatinyadav5004@gmail.com>
2023-01-27 18:06:54 +0530

    first commit

---
+++ query.php
@@ -0,0 +1,24 @@
+<?php
+$conn = mysqli_connect("localhost:3306","root","jatinyadav2001","onlinebot");
+
+// jdbc:mysql://localhost:3306/?user=root
+
+if($conn)
+{
+    $user_messages = mysqli_real_escape_string($conn, $_POST['messageValue']);
+
+    $query = "SELECT * FROM chatbot WHERE messages LIKE '%$user_messages%'";
+    $makeQuery = mysqli_query($conn, $query);
+
+    if(mysqli_num_rows($makeQuery) > 0) 
+    {
+        $result = mysqli_fetch_assoc($makeQuery);
+
+        echo $result['response'];
+    }else{
+        echo "Sorry, I can't understand you.";
+    }
+}else {
+    echo "Connection failed" . mysqli_connect_errno();
+}
+?>
